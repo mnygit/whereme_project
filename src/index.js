42796bc1c1ae14a0b8ce5610ec512e8b5916743c
Marilyn Young <mnyua1@gmail.com>
2021-07-10 15:06:37 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const Test = () => {
+  return (
+    <section>
+      <h2>Style Quiz</h2>
+      <p>What occasion are you dressing for?</p>
+      <p>What is your age range?</p>
+      <p>What is the season?</p>
+      </section>
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+  );
+}
+
+ReactDOM.render(<Test/>, document.getElementById('root'));
 
 
