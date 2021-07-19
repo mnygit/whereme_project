16a587b9894fbbae25e56b57baf56aa47c9fc7c6
Marilyn Young <mnyua1@gmail.com>
2021-07-19 07:47:12 -0400

    added classes to header

--- src/Header.js
+++ src/Header.js
@@ -4,15 +4,20 @@ import './index.css';
 
 const Header = () => {
     return (
-        <div>
-        <nav>
-        <ul>
+        <header className="main-header">
+         <div className="container">
+             <h1 className="logo"> 
+             <img src="https://cdn2.iconfinder.com/data/icons/e-commerce-icons-2/256/Ecommerce_Icons_Rose_Color-11-512.png"/>
+             </h1> 
+        <nav className="main-nav">
+        <ul className="main-nav-list">
         <li><a href="#">About</a></li>
         <li><a href="#">Survey</a></li>
         <li><a href="#">Contact</a></li>
         </ul>
         </nav>
         </div>
+        </header>
     
     );
 }
