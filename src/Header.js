4f0b5af814a1feff68feb9b872597281f2c0f351
Marilyn Young <mnyua1@gmail.com>
2021-07-18 20:07:06 -0400

    added fonts

--- src/Header.js
+++ src/Header.js
@@ -7,9 +7,11 @@ const Header = () => {
         <div>
         <nav>
         <ul>
+        <span className="font-link">
         <li><a href="#">About</a></li>
         <li><a href="#">Survey</a></li>
         <li><a href="#">Contact</a></li>
+        </span>
         </ul>
         </nav>
         </div>
