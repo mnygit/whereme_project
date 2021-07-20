6c68b1117080ab3037381f02eaf0a0d281409616
Marilyn Young <mnyua1@gmail.com>
2021-07-20 18:45:39 -0400

    added react player

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './Footer';
 
 
 
+
 const App = () => {
 return (
    <div>
--- src/Hero.js
+++ src/Hero.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './index.css';
 import './hero_video.mp4';
+import ReactPlayer from 'react-player';
 
 
   const HeroSection = () => {
       return (
         <div className="hero-section">
+         <ReactPlayer url='https://www.youtube.com/watch?v=e90eWYPNtJ8' /> 
         <h2 className="hero">Hero Section</h2>
+
         </div>
       )
   }
