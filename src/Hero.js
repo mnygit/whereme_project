1f80b9519e80bf7e281b481236f059a01589ee1c
Marilyn Young <mnyua1@gmail.com>
2021-07-20 18:22:03 -0400

    hero section

--- src/Hero.js
+++ src/Hero.js
@@ -5,7 +5,7 @@ import './hero_video.mp4';
 
   const HeroSection = () => {
       return (
-        <div>
+        <div className="hero-section">
         <h2 className="hero">Hero Section</h2>
         <video src="hero_video.mp4"/>
         </div>
