150b6be3c104ec826d4432e965b9bd77ffe8468e
Marilyn Young <mnyua1@gmail.com>
2021-07-22 12:24:48 -0400

    renamed video component

--- src/Hero.js
+++ src/Hero.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './index.css';
-import './hero_video.mp4';
+import video from './hero_video.mp4';
 import ReactPlayer from 'react-player';
 
 
   const HeroSection = () => {
       return (
         <div className="hero-section">
-         <ReactPlayer url='./hero_video.mp4' width="100%" height="100%" controls={true}/> 
+         <ReactPlayer url={video} width="50%" height="auto" controls={true}/> 
         <h2 className="hero">Hero Section</h2>
 
         </div>
