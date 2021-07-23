969871a63616c1aac5d07a02c37ac0f64adb0150
Marilyn Young <mnyua1@gmail.com>
2021-07-23 18:56:36 -0400

    form updates

--- src/SurveyForm.js
+++ src/SurveyForm.js
@@ -4,18 +4,17 @@ import './index.css';
 
 const SurveyForm = () => {
     return (
-      <div>
-      <section className="quiz">
+      <article>
+      <form className="quiz">
         <h2>Style Quiz</h2>
         <p>What occasion are you dressing for?</p>
         <p>What is your age range?</p>
         <p>What is the season?</p>
         <button class="btn">Show Styles!</button>
-        </section>
+        </form>
         <div>
-        {/* <button class="btn">Show Styles!</button> */}
         </div>
-        </div>
+        </article>
     );
   }
 
