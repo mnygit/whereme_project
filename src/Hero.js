ee89c3e00dda31a40e3d603e8d00b3fe3c53d625
Marilyn Young <mnyua1@gmail.com>
2021-10-01 11:23:58 -0400

    add intro classes

--- src/Hero.js
+++ src/Hero.js
@@ -7,16 +7,19 @@ import ReactPlayer from 'react-player';
   const HeroSection = () => {
       return (
         <div className="hero-section"> 
-          <div className="intro hero-column"> 
-          <h2>Sed ut perspiciatis unde</h2>
+          <section className="hero-column hero-intro"> 
+          <div className="intro">
+          <h2 className="intro-header">Sed ut perspiciatis unde</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
             labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
             aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
           </div>
+          </section>
+          <section className="hero-column">
          <ReactPlayer
          className="hero-video"
          url={video} 
-         width="50%" 
+         width="100%" 
          height="auto" 
          controls={false}
          playing
@@ -27,6 +30,7 @@ import ReactPlayer from 'react-player';
            muted: true
          }}}}
          />
+         </section>
         </div>
       )
   }
