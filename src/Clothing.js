19212ea8885a105bbac410fe333cf130f82e41c6
Marilyn Young <mnyua1@gmail.com>
2021-10-13 12:25:41 -0400

    create image labels

--- src/Clothing.js
+++ src/Clothing.js
@@ -11,16 +11,29 @@ import baecation from './baecation.png';
 const Clothing = () => {
  return (
     <section className="clothing"> 
-    <h4>What's your occasion?</h4>
-    <p>Lorem ipsum dolor sit amet,Ut enim ad minim veniam.</p>
-    <div className="gallery">  
-    <img src={daily} alt="everyday wear" />
-    <img src={business} alt="business" />
-    <img src={sports} alt="sports" />
-    <img src={formal} alt="formal" />
-    <img src={baecation} alt="swimsuit" />
-    
-    
+    <h4 className="clothing-label">What's your occasion?</h4>
+    <p className="clothing-intro">Lorem ipsum dolor sit amet,Ut enim ad minim veniam.</p>
+    <div className="gallery">
+        <div className="img-one"> 
+    <img className="gallery-img" src={daily} alt="everyday wear" />
+    <div className="img-label">Everyday</div>
+    </div>
+    <div className="img-two">
+    <img className="gallery-img" src={business} alt="business" />
+    <div className="img-label">Business</div>
+    </div>
+    <div className="img-three">
+    <img className="gallery-img" src={sports} alt="sports" />
+    <div className="img-label">Sports</div>
+    </div>
+    <div className="img-four">
+    <img className="gallery-img" src={formal} alt="formal" />
+    <div className="img-label">Formal</div>
+    </div>
+    <div className="img-five">
+    <img className="gallery-img" src={baecation} alt="swimsuit" />
+    <div className="img-label">Baecation</div>
+    </div>
     </div>
     
     
