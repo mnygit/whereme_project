a986e3f05aa0dfd945b8057e631e5ed05cab20ee
Marilyn Young <mnyua1@gmail.com>
2021-10-13 10:11:41 -0400

    move images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import Hero from './Hero';
+import Clothing from './Clothing';
 import SurveyForm from './SurveyForm'; 
 import Footer from './Footer';
 
@@ -13,6 +14,7 @@ return (
    <div>
       <Header />
       <Hero />
+      <Clothing />
       {/* <Footer /> */}
     </div>
   )
--- src/Clothing.js
+++ src/Clothing.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './index.css';
-import {useState} from 'react';
+import daily from './everyday.png';
+
 
 
 const Clothing = () => {
- return ({
-
+ return (
+    <section className="clothing"> 
+    <h4>What's your occasion?</h4>
+    <p>Lorem ipsum dolor sit amet,Ut enim ad minim veniam.</p>
+    <div className="gallery">  
+    <img src={daily} alt="everyday wear" />
     
- })
+    
+    </div>
+    
+    
+    
+    </section>
+
+ )
 
 
 }
