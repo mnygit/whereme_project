5b5b2deae78b548e0cf21135da3dfad4d5b30994
Marilyn Young <mnyua1@gmail.com>
2021-10-13 10:50:11 -0400

    remove old image

--- src/Clothing.js
+++ src/Clothing.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './index.css';
 import daily from './everyday.png';
+import business from './business.png';
+import sports from './sports.png';
+import formal from './formal.png';
+import baecation from './baecation.png';
 
 
 
@@ -11,6 +15,10 @@ const Clothing = () => {
     <p>Lorem ipsum dolor sit amet,Ut enim ad minim veniam.</p>
     <div className="gallery">  
     <img src={daily} alt="everyday wear" />
+    <img src={business} alt="business" />
+    <img src={sports} alt="sports" />
+    <img src={formal} alt="formal" />
+    <img src={baecation} alt="swimsuit" />
     
     
     </div>
