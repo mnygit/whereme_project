d46665af36aa8742a65fac7806c8ff2f16650e19
Marilyn Young <mnyua1@gmail.com>
2021-10-16 17:14:52 -0400

    add quotes

---
+++ src/testimonials.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './index.css';
+
+
+
+const Testimonial = () => {
+
+    return (
+      <section> 
+       <h4>What people are saying</h4>
+      <blockquote><p className="quotation">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p></blockquote>
+
+
+      </section>
+
+
+    )
+}
