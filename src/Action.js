527b2aafa1161cd2bb08c1b4991a0bd4c9adcfc5
Marilyn Young <mnyua1@gmail.com>
2021-10-17 13:29:41 -0400

    add new section

---
+++ src/Action.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './index.css';
+import dress from './dress.jpg';
+
+
+
+const Action = () => {
+    return (
+    <section>
+      <img src={dress} />
+      <button className="btn">Style Me Now!</button>
+
+
+    </section>
+
+
+    )
+}
