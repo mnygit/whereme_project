798dd9eb6e654845f158b612599080729b8574de
Marilyn Young <mnyua1@gmail.com>
2021-10-22 17:26:18 -0400

    resize image

--- src/Action.js
+++ src/Action.js
@@ -7,7 +7,7 @@ import dress from './dress.png';
 const Action = () => {
     return (
     <section>
-      <img src={dress} />
+      <img className="dress" src={dress} />
       <button className="btn">Style Me Now!</button>
 
 
